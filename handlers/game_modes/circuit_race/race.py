b93ee4afe2dac5a7d568f5f03737868ed6d5c6ed
sbabinov <serafimbabinov106@gmail.com>
2024-06-10 01:20:22 +0300

    feat: add waiting for other players after start
    * fix wrong deletion of score dict items

--- handlers/game_modes/circuit_race/race.py
+++ handlers/game_modes/circuit_race/race.py
@@ -9,6 +9,7 @@ from ..modes import CircuitRace, active_players
 from image_generation import get_image
 from image_generation.game_modes.circuit_race.race import *
 from keyboards.game_modes.circuit_race import CircuitRaceKeyboard
+from handlers.common.loading import loading, loading_messages
 
 
 async def start(race: CircuitRace) -> None:
@@ -33,7 +34,13 @@ async def start(race: CircuitRace) -> None:
         keyboard = CircuitRaceKeyboard.start_race_menu(player, 0)
         if not race.penalties[player]:
             await race.edit_keyboard('start', player, keyboard)
-        race.other_data[player] = time.time()
+            race.other_data[player] = time.time()
+
+    begin_time = time.time()
+    while (len(race.other_data) != 0) and (time.time() - begin_time < 8):
+        await asyncio.sleep(1)
+    for player in players:
+        await loading(player, end=True)
 
 
 @dp.callback_query_handler(text_contains='race_start')
@@ -46,7 +53,10 @@ async def player_select_circuit(call: CallbackQuery):
     if (race.score[user_id] == 0) and (not race.other_data.get(user_id)):
         race.penalties[user_id] += 50
         await race.delete_message('start', user_id)
-        return await race.send_message('race_start_result', user_id, 'False start!')
+        await race.send_message('race_start_result', user_id, 'False start!')
+        if race.other_data.get(user_id):
+            del race.other_data[user_id]
+        return await loading(user_id, frames=loading_messages['clock'])
     diff = time.time() - race.other_data[user_id]
     await race.delete_message('start', user_id)
     if diff < 0.5:
@@ -58,4 +68,5 @@ async def player_select_circuit(call: CallbackQuery):
     else:
         race.score[user_id] += 10
         await race.send_message('race_start_result', user_id, 'Not bad!')
-    del race.score[user_id]
+    del race.other_data[user_id]
+    await loading(user_id, frames=loading_messages['clock'])
